1d339e7452236a8f04cdb72fab73bee2a88426e4
Albert Magyar <albert.magyar@gmail.com>
2015-06-29 03:01:38 -0700

    Add CSR file

---
+++ src/verilog/vscale_csr_addr_map.vh
@@ -0,0 +1,53 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+`define CSR_ADDR_CYCLE     12'hC00
+`define CSR_ADDR_TIME      12'hC01
+`define CSR_ADDR_INSTRET   12'hC02
+`define CSR_ADDR_CYCLEH    12'hC80
+`define CSR_ADDR_TIMEH     12'hC81
+`define CSR_ADDR_INSTRETH  12'hC82
+`define CSR_ADDR_MCPUID    12'hF00
+`define CSR_ADDR_MIMPID    12'hF01
+`define CSR_ADDR_MHARTID   12'hF10
+`define CSR_ADDR_MSTATUS   12'h300
+`define CSR_ADDR_MTVEC     12'h301
+`define CSR_ADDR_MTDELEG   12'h302
+`define CSR_ADDR_MIE       12'h304
+`define CSR_ADDR_MTIMECMP  12'h321
+`define CSR_ADDR_MTIME     12'h701
+`define CSR_ADDR_MTIMEH    12'h741
+`define CSR_ADDR_MSCRATCH  12'h340
+`define CSR_ADDR_MEPC      12'h341
+`define CSR_ADDR_MCAUSE    12'h342
+`define CSR_ADDR_MBADADDR  12'h343
+`define CSR_ADDR_MIP       12'h344
+`define CSR_ADDR_CYCLEW    12'h900
+`define CSR_ADDR_TIMEW     12'h901
+`define CSR_ADDR_INSTRETW  12'h902
+`define CSR_ADDR_CYCLEHW   12'h980
+`define CSR_ADDR_TIMEHW    12'h981
+`define CSR_ADDR_INSTRETHW 12'h982
---
+++ src/verilog/vscale_csr_file.v
@@ -0,0 +1,207 @@
+module vscale_csr_file(
+                       input                 clk,
+                       input                 reset,
+                       input [`XPR_LEN-1:0]  addr,
+                       input                 en,
+                       input                 wen,
+                       input [`XPR_LEN-1:0]  wdata,
+                       input [`XPR_LEN-1:0]  rdata,
+                       input                 retire_wb,
+                       input                 exception_wb,
+                       input [`XPR_LEN-1:0]  mepc, 
+                       output [`XPR_LEN-1:0] mtvec
+                       );
+
+   
+
+   reg [63:0]                                cycle_full;
+   reg [63:0]                                time_full;
+   reg [63:0]                                instret_full;
+   reg [5:0]                                 priv_stack;
+   reg [`XPR_LEN-1:0]                        mtvec;
+   reg                                       mtie;
+   reg                                       msie;
+   reg [`XPR_LEN-1:0]                        mtimecmp;
+   reg [63:0]                                mtime_full;
+   reg [`XPR_LEN-1:0]                        mscratch;
+   reg [`XPR_LEN-1:0]                        mepc;
+   reg [3:0]                                 mecode;
+   reg                                       mint;
+   reg [`XPR_LEN-1:0]                        mbadaddr;
+   
+   wire [`XPR_LEN-1:0]                       mcpuid;
+   wire [`XPR_LEN-1:0]                       mimpid;
+   wire [`XPR_LEN-1:0]                       mhartid;
+   wire [`XPR_LEN-1:0]                       mstatus;
+   wire [`XPR_LEN-1:0]                       mtdeleg;
+   wire [`XPR_LEN-1:0]                       mie;
+   wire [`XPR_LEN-1:0]                       mcause;
+   
+   assign mcpuid = (1 << 20) || (1 << 8); // 'I' and 'U' bits set
+   assign mimpid = 32'h8000;
+   assign mhartid = 0;
+   
+   always @(posedge clk) begin
+      if (reset) begin
+         priv_stack <= 6'b000110;
+      end else if (wen_internal && addr == `CSR_ADDR_MSTATUS) begin
+         priv_stack <= wdata[5:0];
+      end else if (trap) begin
+         // no delegation to U means all traps go to M
+         priv_stack <= {priv_stack[2:0],2'b11,1'b0};
+      end
+   end
+   
+   // this implementation has SD, VM, MPRV, XS, and FS set to 0
+   assign mstatus = {26'b0, priv_stack};
+
+   assign mtdeleg = 0;
+
+   always @(posedge clk) begin
+      if (reset) begin
+         mtip <= 0;
+         msip <= 0;
+      end else begin 
+         if (timer_expired)
+           mtip <= 1;
+         if (wen_internal && addr == `CSR_ADDR_MTIMECMP)
+           mtip <= 0;
+         if (wen_internal && addr == `CSR_ADDR_MIP) begin
+            mtip <= wdata[7];
+            msip <= wdata[3];
+         end
+      end // else: !if(reset)
+   end // always @ (posedge clk)
+   assign mip = {mtip,3'b0,msip,3'b0};
+   
+
+   always @(posedge clk) begin
+      if (reset) begin
+         mtie <= 0;
+         msie <= 0;
+      end else if (wen_internal && addr == `CSR_ADDR_MIE) begin
+         mtie <= wdata[7];
+         msie <= wdata[3];
+      end
+   end // always @ (posedge clk)
+   assign mie = {mtie,3'b0,msie,3'b0};
+   
+   always @(posedge clk) begin
+      if (exception_WB || interrupt_taken)
+        mepc <= PC_WB && {{30{1'b1}},2'b0};      
+      if (wen_internal && addr == `CSR_ADDR_MEPC)
+        mepc <= wdata && {{30{1'b1}},2'b0};
+   end
+
+   always @(posedge clk) begin
+      if (reset) begin
+         mecode <= 0;
+         mint <= 0;
+      end else if (wen_internal && addr == `CSR_ADDR_MCAUSE) begin
+         mecode <= wdata[3:0];
+         mint <= wdata[31];
+      end else begin
+         if (interrupt_taken) begin
+            mecode <= interrupt_code;
+            mint <= 1'b1;
+         end else if (exception_WB) begin 
+            mecode <= exception_code_WB;
+            mint <= 1'b0;
+         end
+      end // else: !if(reset)
+   end // always @ (posedge clk)
+   assign mcause = {mint,27'b0,mecode};
+
+   assign code_imem = (exception_code_WB == `ECODE_INST_ADDR_MISALIGNED)
+     || (exception_code_WB == `ECODE_INST_ADDR_MISALIGNED);
+   
+   always @(posedge clk) begin
+      if (exception_WB)
+        mbadaddr <= (code_imem) ? PC_WB : alu_out_WB;
+      if (wen_internal && addr == `CSR_ADDR_MBADADDR)
+        mbadaddr <= wdata;
+   end    
+   
+   always @(*) begin
+      case (addr)
+        `CSR_ADDR_CYCLE     : begin rdata = cycle_full[31:0]; defined = 1'b1; end
+        `CSR_ADDR_TIME      : begin rdata = time_full[31:0]; defined = 1'b1; end
+        `CSR_ADDR_INSTRET   : begin rdata = instret_full[31:0]; defined = 1'b1; end
+        `CSR_ADDR_CYCLEH    : begin rdata = cycle_full[63:32]; defined = 1'b1; end
+        `CSR_ADDR_TIMEH     : begin rdata = time_full[63:32]; defined = 1'b1; end
+        `CSR_ADDR_INSTRETH  : begin rdata = instret_full[63:32]; defined = 1'b1; end
+        `CSR_ADDR_MCPUID    : begin rdata = mcpuid; defined = 1'b1; end
+        `CSR_ADDR_MIMPID    : begin rdata = mimpid; defined = 1'b1; end
+        `CSR_ADDR_MHARTID   : begin rdata = mhartid; defined = 1'b1; end
+        `CSR_ADDR_MSTATUS   : begin rdata = mstatus; defined = 1'b1; end
+        `CSR_ADDR_MTVEC     : begin rdata = mtvec; defined = 1'b1; end
+        `CSR_ADDR_MTDELEG   : begin rdata = mtdeleg; defined = 1'b1; end
+        `CSR_ADDR_MIE       : begin rdata = mie; defined = 1'b1; end
+        `CSR_ADDR_MTIMECMP  : begin rdata = mtimecmp; defined = 1'b1; end
+        `CSR_ADDR_MTIME     : begin rdata = mtime_full[31:0]; defined = 1'b1; end
+        `CSR_ADDR_MTIMEH    : begin rdata = mtime_full[63:32]; defined = 1'b1; end
+        `CSR_ADDR_MSCRATCH  : begin rdata = mscratch; defined = 1'b1; end
+        `CSR_ADDR_MEPC      : begin rdata = mepc; defined = 1'b1; end
+        `CSR_ADDR_MCAUSE    : begin rdata = mcause; defined = 1'b1; end
+        `CSR_ADDR_MBADADDR  : begin rdata = mbadaddr; defined = 1'b1; end
+        `CSR_ADDR_MIP       : begin rdata = mip; defined = 1'b1; end
+        `CSR_ADDR_CYCLEW    : begin rdata = cycle_full[31:0]; defined = 1'b1; end
+        `CSR_ADDR_TIMEW     : begin rdata = time_full[31:0]; defined = 1'b1; end
+        `CSR_ADDR_INSTRETW  : begin rdata = instret_full[31:0]; defined = 1'b1; end
+        `CSR_ADDR_CYCLEHW   : begin rdata = cycle_full[63:32]; defined = 1'b1; end
+        `CSR_ADDR_TIMEHW    : begin rdata = time_full[63:32]; defined = 1'b1; end
+        `CSR_ADDR_INSTRETHW : begin rdata = instret_full[63:32]; defined = 1'b1; end
+        default : begin rdata = 0; defined = 1'b0; end
+      endcase // case (addr)
+   end // always @ (*)
+   
+   
+   always @(posedge clk) begin
+      if (reset) begin
+         cycle_full <= 0;
+         time_full <= 0;
+         instret_full <= 0;
+         mtime_full <= 0;
+      end else begin
+         cycle_full <= cycle_full + 1;
+         time_full <= time_full + 1;
+         if (retire_wb)
+           instret_full <= instret_full + 1;
+         mtime_full <= mtime_full + 1;
+      end // else: !if(reset)
+      if (wen_internal) begin
+         case (addr)
+           `CSR_ADDR_CYCLE     : cycle_full[31:0] <= wdata;
+           `CSR_ADDR_TIME      : time_full[31:0] <= wdata;
+           `CSR_ADDR_INSTRET   : instret_full[31:0] <= wdata;
+           `CSR_ADDR_CYCLEH    : cycle_full[63:32] <= wdata;
+           `CSR_ADDR_TIMEH     : time_full[63:32] <= wdata;
+           `CSR_ADDR_INSTRETH  : instret_full[63:32] <= wdata;
+           // mcpuid is read-only
+           // mimpid is read-only
+           // mhartid is read-only
+           // mstatus handled separately
+           `CSR_ADDR_MTVEC     : mtvec <= wdata && {{30{1'b1}},2'b0};
+           // mtdeleg constant
+           // mie handled separately
+           `CSR_ADDR_MTIMECMP  : mtimecmp <= wdata;
+           `CSR_ADDR_MTIME     : mtime_full[31:0] <= wdata;
+           `CSR_ADDR_MTIMEH    : mtime_full[63:32] <= wdata;
+           `CSR_ADDR_MSCRATCH  : mscratch <= wdata;
+           // mepc handled separately
+           // mcause handled separately
+           // mbadaddr handled separately
+           // mip handled separately
+           `CSR_ADDR_CYCLEW    : cycle_full[31:0] <= wdata;
+           `CSR_ADDR_TIMEW     : time_full[31:0] <= wdata;
+           `CSR_ADDR_INSTRETW  : instret_full[31:0] <= wdata;
+           `CSR_ADDR_CYCLEHW   : cycle_full[63:32] <= wdata;
+           `CSR_ADDR_TIMEHW    : time_full[63:32] <= wdata;
+           `CSR_ADDR_INSTRETHW : instret_full[63:32] <= wdata;
+         endcase // case (addr)
+      end // if (wen_internal)
+   end // always @ (posedge clk)
+
+   
+  
+endmodule // vscale_csr_file
